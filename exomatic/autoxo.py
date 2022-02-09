b304863daca87b5bd6729a24301611e2b77b3a96
Vedad Hodzic <vxh710@bham.ac.uk>
2022-02-09 16:44:59 -0700

    rv and phot works

--- exomatic/autoxo.py
+++ exomatic/autoxo.py
@@ -22,17 +22,38 @@ def _set_default(value, default=0.1, sd=1):
     elif isinstance(value, list):
         return value
 
+def concatenate_array(x, index):
+    return np.concatenate(
+            [
+                item[i] for item in lst
+                for _,lst in x.items()
+                ]
+            )
+
+def _set_multiplanet(value, n=1):
+    if n == 1:
+        return value
+    return np.array([value] * n)
+
+def plot_fit(res):
+    return
+
+
 
 def automodel(
+        """
+        docstr
+        """
         dataset_lc       = None,
         dataset_rv       = None,
         period           = None,
         t0               = None,
         tp               = None,
+#        transiting       = None,
         eccentric        = False,
+        trend_order      = 0,
         optimize         = True,
             ):
-
 
     if isinstance(period, float):
         period = [period]
@@ -43,6 +64,9 @@ def automodel(
 
     n_pl = len(period)
 
+#    if transiting is None:
+#        transiting = [True] * n_pl
+
     dur    = _set_default(None, sd=5)
     ror    = _set_default(None, sd=5)
 #    dur    = [_set_default(None, sd=5) for _ in range(n_pl)]
@@ -50,30 +74,50 @@ def automodel(
 #    period = [np.array(_set_default(value)) for value in period])
     period = np.array([period[0]])
     t0     = _set_default(t0[0])
-
+    
+#    print('period', period)
     res = dict()
 
 
     if dataset_lc is not None:
+        fit_lc = True
 
         res['lc'] = dict()
 
-        fit_lc = True
 #        lcres         = dict()
-        res['lc']['gp']   = dict()
-        res['lc']['lc']   = dict()
-        res['lc']['mean'] = dict()
+        # computed GP model on observations per instrument
+#        res['lc']['gp']   = dict()
+#        res['lc']['lc']   = dict()
+
+        # full model on observations per instrument
+        res['lc'] = dict() # res['lc']['tess_0'] etc
+
+        # gp predictive model per ``instrument``
+#        res['lc']['gp_pred'] = dict() # res['lc']['gp_pred']['tess_0']
+        res['lc']['gp'] = dict() # res['lc']['gp_pred']['tess_0']
+
+        # predictive model per ``instrument`` (really band, but because of texp
+        # it needs to be per instrument
+        res['lc']['lc_pred'] = dict() # res['lc']['lc_pred']['tess']
+        res['lc']['lc_phase_pred'] = dict() # res['lc']['lc_phase_pred']['tess']
+
     else:
         fit_lc = False
 
     if dataset_rv is not None:
 
-        res['rv'] = dict()
+        # pred should have bkg and rv
+        # obs should have total
         fit_rv = True
-#        rvres          = dict()
-        res['rv']['rv']    = dict()
-#        rvres['trend'] = dict()
-        res['rv']['mean']  = dict()
+
+        # full model on observations per instrument
+        res['rv'] = dict() # res['rv']['harps']
+
+        # predictive models for system, single function, not dict
+#        res['rv']['rv_pred']
+#        res['rv']['rv_phase_pred']
+#        res['rv']['bkg_pred']
+
 
     else:
         fit_rv = False
@@ -81,8 +125,6 @@ def automodel(
 
     with pm.Model() as model:
 
-#        _period = pm.Lognormal("period", mu=np.log(period[0]), 
-#                sd=period[1], shape=n_pl)
         _period = pm.Lognormal("period", mu=np.log(period), 
                 sd=np.array([1]), shape=n_pl)
 
@@ -95,8 +137,6 @@ def automodel(
 
         if fit_rv:
             K = pm.Lognormal("K", mu=np.log(0.05), sigma=5)
-#            trend = pm.Normal("trend", mu=0, sd=10.0 ** -np.arange(1,3)[::-1],
-#                    shape=2)
 
         if eccentric:
             _ecs = pmx.UnitDisk("ecs", testval=np.array([0.01, -0.01]))
@@ -115,6 +155,15 @@ def automodel(
            b        = _b
         )
 
+        orbit_phase = xo.orbits.KeplerianOrbit(
+           duration = _dur,
+           period   = _period,
+           ecc      = _ecc,
+           omega    = _omega,
+           t0       = 0,
+           b        = _b
+        )
+
 
         u = dict()
         for name, _ in dataset_lc.items():
@@ -123,72 +172,138 @@ def automodel(
 
         parameters = dict()
 
+
         if fit_rv:
+
+            if trend_order > 0:
+
+                t_tot = np.concatenate(
+                        [
+                            item[0] for item in list(dataset_lc.values())[0]
+                            ]
+                        )
+                t_ref = 0.5 * (t_tot.min() + t_tot.max())
+
+                if trend_order == 1:
+                    trend = pm.Normal('trend', mu=0, sd=0.1)
+
+                elif trend_order == 2:
+                    trend = pm.Normal('trend', mu=np.array([0, 0]), 
+                            sd=np.array([0.01, 0.1]), shape=2)
+
+                else:
+                    raise notimplementederror(
+                    """
+                    polynomial orders higher than 2 is not implemented
+                    """
+                    )
+
+
+            def get_rv_phase_pred(t):
+                return orbit_phase.get_radial_velocity(t, K * 1e3) 
+
+            def get_rv_pred(t):
+                return orbit.get_radial_velocity(t, K * 1e3)
+
+            def get_bkg_model_pred(t):
+                if trend_order > 0:
+
+                    if trend_order == 1:
+                        A = np.vander((t - t_ref), 2)[:,0]
+
+                    elif trend_order == 2:
+                        A = np.vander((t - t_ref), 3)[:,:2]
+
+                    return tt.dot(A, trend)
+
+                else:
+                    return tt.zeros_like(t)
+
+
+            # save predictive functions
+            res['rv']['rv_pred']       = get_rv_pred
+            res['rv']['rv_phase_pred'] = get_rv_phase_pred
+            res['rv']['bkg_pred']      = get_bkg_model_pred
+
+
             for i, (name, (x, y, yerr)) in enumerate(dataset_rv.items()):
 
                 subname = f"{name}"
 
-                with pm.Model(name=subname, model=model):
+                with pm.Model(model=model) as submodel:
                 
-                    _mean_rv   = pm.Normal("mean", mu=np.median(y),
+                    _mean_rv   = pm.Normal(f"mean_{subname}", mu=np.median(y),
                             sd=0.5)
-                    _sigma_rv = pm.HalfNormal("sigma", sd=0.1)
+                    _sigma_rv = pm.HalfNormal(f"sigma_{subname}", sd=0.1)
 
-                    parameters[f'{subname}_noise'] = [_mean_rv, _sigma_rv]
+                    parameters[f'noise_{subname}'] = [_mean_rv, _sigma_rv]
 
-                def radial_velocity(mean, K, t):
-                    vrad = orbit.get_radial_velocity(t, K * 1e3)
-                    if n_pl > 1:
-                        return tt.sum(vrad, axis=-1) / 1e3 + mean
-                    return vrad / 1e3 + mean
+                    res[f'model_{subname}'] = submodel
 
-                radial_velocity = partial(radial_velocity, _mean_rv, K)
-                res['rv']['rv'][subname] = radial_velocity
 
-                rv_model = radial_velocity(x)
-                pm.Normal(f"{subname}_obs",
-                        mu       = radial_velocity(x),
+                rvmod = (
+                        orbit.get_radial_velocity(x, K * 1e3) / 1e3 + 
+                        _mean_rv +
+                        get_bkg_model_pred(x)
+                        )
+
+                res['rv'][subname] = rvmod
+
+                pm.Normal(f"obs_{subname}",
+                        mu       = rvmod,
                         sd       = tt.sqrt(yerr**2 + _sigma_rv**2),
                         observed = y
                         )
-    #                x_rv_ref = 0.5 * (x.min() + x.max())
-    #                def radial_velocity_trend(trend, t):
-    #                    A = np.vander(t - x_rv_ref, 3)[:,:2]
-    #                    bkg = tt.dot(A, trend)
 
 
         if fit_lc:
             for name, data in dataset_lc.items():
 
+                star = xo.LimbDarkLightCurve(u[name])
+
+                def get_lc_phase_pred(t, texp=None):
+                    return (tt.sum(
+                                    star.get_light_curve(
+                                        orbit = orbit_phase,
+                                        r     = _ror,
+                                        t     = t,
+                                        texp  = texp
+                                        ), axis=-1
+                                    ) + 1)
+
+                def get_lc_pred(t, texp=None):
+                    return (tt.sum(
+                                    star.get_light_curve(
+                                        orbit = orbit,
+                                        r     = _ror,
+                                        t     = t,
+                                        texp  = texp
+                                        ), axis=-1
+                                    ) + 1)
+
+                res['lc']['lc_pred'][name] = get_lc_pred
+                res['lc']['lc_phase_pred'][name] = get_lc_phase_pred
+
                 for i, (x, y, yerr, texp) in enumerate(data):
 
                     subname = f"{name}_{i}"
 
-                    with pm.Model(name=subname, model=model):
+                    with pm.Model(model=model) as submodel:
 
-                        _mean     = pm.Lognormal("mean", mu=np.log(np.median(y)), sd=5)
-                        _sigma_gp = pm.Lognormal("sigma_gp", mu=np.log(np.std(y)), sd=10)
-                        _rho_gp   = pm.Lognormal("rho_gp", mu=0, sd=10)
-                        _sigma    = pm.Lognormal("sigma", mu=np.log(np.var(y)), sd=10)
+                        _mean     = pm.Lognormal(f"mean_{subname}", mu=np.log(np.median(y)), sd=5)
+                        _sigma_gp = pm.Lognormal(f"sigma_gp_{subname}", mu=np.log(np.std(y)), sd=10)
+                        _rho_gp   = pm.Lognormal(f"rho_gp_{subname}", mu=0, sd=10)
+                        _sigma    = pm.Lognormal(f"sigma_{subname}", mu=np.log(np.var(y)), sd=10)
 
-                        parameters[f'{subname}_noise'] = [_mean, _sigma_gp, 
+                        parameters[f'noise_{subname}'] = [_mean, _sigma_gp, 
                                                          _rho_gp, _sigma]
 
-                    star = xo.LimbDarkLightCurve(u[name])
-
-                    def light_curve(mean, star, ror, texp, t):
-                        return (tt.sum(
-                                    star.get_light_curve(
-                                        orbit = orbit,
-                                        r     = ror,
-                                        t     = t,
-                                        texp  = texp
-                                        ), axis=-1
-                                    ) + 1) * mean
+                        res[f'model_{subname}'] = submodel
 
 
-                    light_curve = partial(light_curve, _mean, star, _ror, texp)
-                    res['lc']['lc'][subname] = light_curve
+                    lcmod = get_lc_pred(x, texp=texp) * _mean
+
+                    res['lc'][subname] = lcmod
 
 
                     kernel = terms.SHOTerm(
@@ -200,12 +315,13 @@ def automodel(
                     gp = GaussianProcess(
                             kernel, 
                             t     = x,
-                            diag  = light_curve(x)**2 * _sigma**2 + yerr**2,
-                            mean  = light_curve,
+#                            diag  = light_curve(x)**2 * _sigma**2 + yerr**2,
+                            diag  = lcmod**2 * _sigma**2 + yerr**2,
+#                            mean  = lcmod,
                             quiet = True
                             )
                         
-                    gp.marginal(f"{subname}_obs", observed=y)
+                    gp.marginal(f"obs_{subname}", observed=y-lcmod)
 
                     res['lc']['gp'][subname] = gp
 
@@ -223,6 +339,9 @@ def automodel(
                 map_soln = xo.optimize(start=map_soln, vars=[ecs, K])
             else:
                 map_soln = xo.optimize(start=map_soln, vars=[ecs])
+
+        if trend_order > 0:
+            map_soln = xo.optimize(start=map_soln, vars=[trend])
 
         map_soln = xo.optimize(start=map_soln, vars=[_b] + list(u.values()))
 
@@ -246,11 +365,6 @@ def automodel(
 
     res['model']    = model
     res['map_soln'] = map_soln
-
-#    if fit_lc and not fit_rv:
-#        return lcres
-#    if fit_rv and not fit_lc:
-#        return rvres
 
     return res
 
